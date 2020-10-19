fad93c1a279079cff40cec4259438892c3e2c965
rmoschler <r.moschler@ludus-academie.fr>
2020-10-19 11:11:40 +0200

    'premier_push_site_web'

---
+++ js/Tableau_jeu.js
@@ -0,0 +1,11 @@
+function afficheDiv()
+{
+	if (document.getElementById("Genre_jeu").style.visibility == "visible") 
+	{
+		document.getElementById("Genre_jeu").style.visibility = "hidden";
+	}
+	else
+	{
+		document.getElementById("Genre_jeu").style.visibility = "visible";
+	}
+}
